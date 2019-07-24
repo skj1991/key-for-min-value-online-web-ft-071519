2027d9c6e2249c54d788b67c1dcfcf3e7feea7d8
Stacy Johns <stacykjohns@icloud.com>
2019-07-24 11:40:29 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,11 +1,11 @@
 def key_for_min_value(name_hash)
-  name_a = name_hash.to_a 
+  name_a = name_hash.to_a
 
-  d_value= 1000 
+  d_value= 1000
   d_key= nil
 
   name_a.each do |n|
-      if n[1] < d_value 
+      if n[1] < d_value
         d_value = n[1]
         d_key = n[0]
       end
